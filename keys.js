7f1f29296743e80581440b7748852e950c2eba22
Brian Dunams <dabadestboy1@gmail.com>
2017-07-09 18:44:20 -0400

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 const twitterKeys = {
-  consumer_key: 'k1TfXbpB9URx7tAJx91qmP9sx',
-  consumer_secret: 't1hUTlbIJRV8Qq93uMh5nrXmzNWPATcJ3hPbQWPDvhJvO7OB3T',
-  access_token_key: '263336530-JFO0VQloapmw2NTkjiQ7UFcSDzoxBagadzfgNU15',
-  access_token_secret: 'zmSf4uCoVz67i7bk3HL5brp9HhGCHCr4hcTdmjGSwdUOC'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 }
 
 const spotifyKeys = {
-  clientId : 'ec96a95d1d8a46eb9a7f17586a188e28',
-  clientSecret : '6dfe082193af4e64a9f76819117530f0O',
-  redirectUri: 'http://localhost:8888/callback'
+  clientId : '',
+  clientSecret : '',
+  redirectUri: ''
 }
 
 
